b3404df10119f2122088b9e2f351eec8422897fc
William Butts <wbutts1023@gmail.com>
2015-10-26 18:08:49 -0400

    trying to get second level urls, not working yet

--- stattracker_api/api/views.py
+++ stattracker_api/api/views.py
@@ -1,9 +1,16 @@
 from rest_framework import viewsets
 from .models import Activity
 from .serializers import ActivitySerializer
+from datetime import datetime
 
 # Create your views here.
 
 class ActivityViewSet(viewsets.ModelViewSet):
     queryset = Activity.objects.all()
     serializer_class = ActivitySerializer
+
+class StatsViewSet(viewsets.ModelViewSet):
+    serializers_class = ActivitySerializer
+
+    def get_queryset(self):
+        return Activity.objects.filter(activity_date=datetime.date)
--- stattracker_api/stattracker_api/urls.py
+++ stattracker_api/stattracker_api/urls.py
@@ -16,13 +16,17 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from rest_framework import routers
-from api.views import ActivityViewSet
+from api.views import ActivityViewSet, StatsViewSet
 
-router = routers.DefaultRouter()
+router = routers.SimpleRouter()
 router.register(r'activities', ActivityViewSet)
+
+# activies_router = routers.NestedSimpleRouter(router, r'activities', lookup='activities')
+# activies_router.register(r'stats', views.StatsViewSet)
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^docs/', include('rest_framework_swagger.urls')),
     url(r'^api/', include(router.urls)),
+    # url(r'^api/', include(activies_router.urls)),
 ]
