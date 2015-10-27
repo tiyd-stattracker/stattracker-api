6ecd88d86eaaa2e0ca839d1bc8246bb62ae3d46b
William Butts <wbutts1023@gmail.com>
2015-10-27 16:58:02 -0400

    trying to massage migrations

--- stattracker_api/api/migrations/0001_initial.py
+++ stattracker_api/api/migrations/0001_initial.py
@@ -13,10 +13,18 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Activity',
             fields=[
-                ('id', models.AutoField(serialize=False, auto_created=True, verbose_name='ID', primary_key=True)),
+                ('id', models.AutoField(verbose_name='ID', auto_created=True, serialize=False, primary_key=True)),
                 ('activity_name', models.CharField(max_length=255)),
+                ('start_date', models.DateField()),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Log',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', auto_created=True, serialize=False, primary_key=True)),
                 ('activity_date', models.DateField()),
                 ('activity_count', models.PositiveSmallIntegerField()),
+                ('activity', models.ForeignKey(related_name='logs', to='api.Activity')),
             ],
         ),
     ]
